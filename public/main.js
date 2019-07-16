46fed782d1032c307ca0ecfb3e7d62a457e9fc91
kakitaku00 <webtrakakinuma@gmail.com>
2019-07-16 16:54:15 +0900

    add google login & loading

---
+++ public/main.js
@@ -0,0 +1,54 @@
+async function initAuth() {
+  const auth = firebase.auth();
+
+  const $app = document.getElementById('app');
+  const $load = document.getElementById('load');
+
+  const $authButton = document.getElementById('auth-button');
+
+  let loginUser = null;
+  let loaded = false;
+
+  auth.onAuthStateChanged(user => {
+    if(!loaded) {
+      loaded = true;
+      $load.style.display =  'none';
+      $app.style.display = '';
+    }
+    console.log('@@@', user);
+
+    loginUser = user;
+    // loginBtnText
+    if (user) {
+      $authButton.innerText = 'Logout';
+    } else {
+      $authButton.innerText = 'Login';
+    }
+  })
+
+  $authButton.addEventListener('click', () => {
+    if(loginUser) {
+      auth.signOut();
+      return;
+    }
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithRedirect(provider);
+  })
+
+  try {
+    const result = await auth.getRedirectResult();
+  } catch(err) {
+    alert(err.message);
+    console.error(err);
+  }
+}
+
+
+async function main() {
+  await initAuth();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  main().catch(err => console.error(err));
+})
+
